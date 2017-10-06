f8e32e8271d63a5871c4380133dffdfa68afd85f
basantos <santosbrigitte@gmail.com>
2017-10-05 20:47:53 -0700

    Add switch turns mechanism

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var playerOnesTurn = true;
+var playerTwosTurn = false;
+
 function setUpGame(){
-  html = '<div id="game-container">';
+  html = "<div><h2 id='show-which-players-turn'>Player One's Turn</h2></div>";
+  html += '<div id="game-container">';
   html += '<div class="row"><div class="unit" id="one"></div><!----><div class="unit" id="two"></div><!----><div class="unit" id="three"></div><!----></div>';
   html += '<div class="row"><div class="unit" id="four"></div><!----><div class="unit" id="five"></div><!----><div class="unit" id="six"></div><!----></div>';
   html += '<div class="row"><div class="unit" id="seven"></div><!----><div class="unit" id="eight"></div><!----><div class="unit" id="nine"></div><!----></div></div>';
@@ -7,6 +11,18 @@ function setUpGame(){
   var mainContainer = document.getElementById('main-container');
   mainContainer.innerHTML = html;
   initGameUnits();
+}
+
+function switchTurn(){
+  playerOnesTurn = !playerOnesTurn;
+  playerTwosTurn = !playerTwosTurn;
+
+  var showWhichPlayersTurn = document.getElementById('show-which-players-turn');
+  if (showWhichPlayersTurn.innerHTML === "Player One's Turn"){
+    showWhichPlayersTurn.innerHTML = "Player Two's Turn";
+  } else {
+    showWhichPlayersTurn.innerHTML = "Player One's Turn";
+  }
 }
 
 var initPlayerButtons = function(){
@@ -25,7 +41,13 @@ var initGameUnits = function(){
   for(var i=0; i<unitIds.length; i++){
     let unitId = document.getElementById(unitIds[i]);
     unitId.addEventListener('click',function(){
-      unitId.innerHTML = 'X';
+      if(playerOnesTurn){
+        unitId.innerHTML = 'X';
+        switchTurn();
+      } else {
+        unitId.innerHTML = 'O';
+        switchTurn();
+      }
     });
   }
 }
