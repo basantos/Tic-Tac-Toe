8b4436d405d0868a2f5c60514e938d9f951a6dd7
basantos <santosbrigitte@gmail.com>
2017-10-08 17:13:28 -0700

    Add win condition and new game mechanism

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var playerOnesTurn = true;
 var playerTwosTurn = false;
+var board = { 'one': 'empty', 'two': 'empty', 'three': 'empty',
+              'four': 'empty', 'five': 'empty', 'six': 'empty',
+              'seven': 'empty', 'eight': 'empty', 'nine': 'empty'};
 
 function setUpGame(){
   html = "<div><h2 id='show-which-players-turn'>Player One's Turn</h2></div>";
@@ -26,6 +29,11 @@ function switchTurn(){
 }
 
 function initPlayerButtons(){
+  // add buttons html
+  var mainContent = document.getElementById('main-container');
+  var htmlOfPromptButtons = '<button id="one-player-button">1 Player</button><button id="two-players-button">2 Players</button>';
+  mainContent.innerHTML = htmlOfPromptButtons;
+  // set up click events
   var onePlayerButton = document.getElementById('one-player-button');
   var twoPlayersButton = document.getElementById('two-players-button');
   onePlayerButton.addEventListener('click', function(){
@@ -36,12 +44,7 @@ function initPlayerButtons(){
   });
 };
 
-var board = { 'one': 'empty', 'two': 'empty', 'three': 'empty',
-              'four': 'empty', 'five': 'empty', 'six': 'empty',
-              'seven': 'empty', 'eight': 'empty', 'nine': 'empty'};
-
 function markUnit(unit,idNumber){
-  debugger;
   if(board[idNumber] === 'empty'){
     if(playerOnesTurn){
       unit.innerHTML = 'X';
@@ -50,7 +53,7 @@ function markUnit(unit,idNumber){
       unit.innerHTML = 'O';
       board[idNumber] = 'O';
     }
-    switchTurn();
+    checkWinCondition();
   }
 }
 
@@ -61,6 +64,31 @@ function initGameUnits(){
     unit.addEventListener('click',function(){
       markUnit(unit,unitIds[i]);
     });
+  }
+}
+
+function checkWinCondition(){
+      // Check rows
+  if (board.one + board.two + board.three === 'XXX' || board.one + board.two + board.three === 'OOO' ||
+      board.four + board.five + board.six === 'XXX' || board.four + board.five + board.six === 'OOO' ||
+      board.seven + board.eight + board.nine === 'XXX' || board.seven + board.eight + board.nine === 'OOO' ||
+      // Check columns
+      board.one + board.four + board.seven === 'XXX' || board.one + board.four + board.seven === 'OOO'  ||
+      board.two + board.five + board.eight === 'XXX' || board.two + board.five + board.eight === 'OOO' ||
+      board.three + board.six + board.nine === 'XXX' || board.three + board.six + board.nine === 'OOO' ||
+      // Check diagonals
+      board.one + board.five + board.nine === 'XXX' || board.one + board.five + board.nine === 'OOO' ||
+      board.three + board.five + board.seven === 'XXX' || board.three + board.five + board.seven === 'OOO'
+    ){
+    var winningPlayer = document.getElementById('show-which-players-turn').innerHTML.slice(0,10);
+    var mainContainer = document.getElementById('main-container');
+    mainContainer.innerHTML = winningPlayer + ' Win' + '<div><button id="new-game-button">New Game</button></div>';
+    var newGameButton = document.getElementById('new-game-button');
+    newGameButton.addEventListener('click', function(){
+      initPlayerButtons();
+    });
+  } else {
+    switchTurn(); // If game hasn't ended, switch players' turn
   }
 }
 
